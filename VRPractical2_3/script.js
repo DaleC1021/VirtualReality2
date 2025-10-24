f158240fa99362d888736f046a30cf36e79a004b
DaleC1021 <tingchenc@nycstudents.net>
2025-10-24 14:36:26 +0000

    :3

--- VRPractical2_3/script.js
+++ VRPractical2_3/script.js
@@ -10,4 +10,17 @@ window.addEventListener("DOMContentLoaded",function() {
     let tree = new Tree(x, 0 , z);
   }
 
+  for (let i = 0; i < 200; i++){
+    let x = rnd(-55,100);
+    let y = rnd(10,60);
+    let z = rnd(-100,50);
+    let cloud = new Cloud(x,y,z);
+  }
+
+  for(let i = 0; i < 500; i++){
+    let x = rnd(-50,50);
+    let z = rnd(-40,40);
+    let house = new House(x, z);
+  }
+
 })
