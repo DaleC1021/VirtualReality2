0c0f7ec811d257e9f52be472e2e92d9f4d85a539
DaleC1021 <tingchenc@nycstudents.net>
2025-10-29 14:58:32 +0000

    :3

--- VRPractical2_4/script.js
+++ VRPractical2_4/script.js
@@ -1,18 +1,17 @@
 let rnd = (l,u) => Math.floor(Math.random()*(u-l) + l);
 let scene, rocket;
 
-window.addEventListener("DOMContentLoaded",function() {
+window.addEventListener("DOMContentLoaded",function(){
   scene = document.querySelector("a-scene"); 
-  
-  rocket=this.document.querySelector("#rocket");
-  rocket.posY=1;
-  rocket.dPosY=0.3;
-
+  let rocket = new rocket(0, 2, -3);
+  for(let i = 0; i < 100; i++){
+		let rocket = new rocket(...);
+		rocket.push(rocket);
   loop();
 })
 
 function loop(){
-  loopRocket();
+  rocket.ascend();
   
   window.requestAnimationFrame( loop );
 }
