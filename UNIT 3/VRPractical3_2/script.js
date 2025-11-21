7ac8861d32e6cf24a39144618b2eeba71d3e7727
DaleC1021 <tingchenc@nycstudents.net>
2025-11-21 14:30:40 +0000

    :3

--- UNIT 3/VRPractical3_2/script.js
+++ UNIT 3/VRPractical3_2/script.js
@@ -1,16 +1,29 @@
-let scene;
+let scene, ufo=[], rocket=[], tree=[], house;
 
 
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene")
+  for(let i=0; i<100; i++){
+    let rocket = new Rocket(rnd(-50,50), rnd(-1,-10), rnd(-50,50));
+    rockets.push(rocket);
+  }
 
+  for(let i=0; i<100; i++){
+    let ufo = new Ufo(rnd(-50,50), rnd(10,30), rnd(-50,50), rnd(3, 30)*0.01);
+    ufos.push(ufo);
+  }
 
-      
   loop();
 })
 
 function loop(){
+  for(let rocket of rockets){
+    rocket.ascend();
+  }
 
+  for(let ufo of ufos){
+    ufo.invade();
+  }
     
   window.requestAnimationFrame(loop);
 }
